816fffff26b646954e88e553b6e4d309b1a49ead
arsalan <amapps58@gmail.com>
2022-09-20 17:57:03 +0430

    initial

---
+++ example/lib/expandable_fab.dart
@@ -0,0 +1,254 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+
+class ExpandableFab extends StatefulWidget {
+  const ExpandableFab({
+    this.initialOpen,
+    required this.distance,
+    required this.children,
+  });
+
+  final bool? initialOpen;
+  final double distance;
+  final List<Widget> children;
+
+  @override
+  State<ExpandableFab> createState() => _ExpandableFabState();
+}
+
+class _ExpandableFabState extends State<ExpandableFab>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // FlutterOverlayWindow.resizeOverlay(340, 335);
+
+    _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggle() async {
+    // if (_open) {
+    //   Timer(Duration(seconds: 2), () {
+    //     FlutterOverlayWindow.resizeOverlay(180, 185);
+    //   });
+    // } else {
+    //   await FlutterOverlayWindow.resizeOverlay(340, 335);
+    //   await Future.delayed(Duration(seconds: 1));
+    // }
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        print("Expandable open");
+
+        _controller.forward();
+      } else {
+        print("Expandable closed");
+        _controller.reverse();
+      }
+    });
+    // await Future.delayed(Duration(seconds: 1));
+    // if (!_open) await FlutterOverlayWindow.resizeOverlay(200, 200);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      // child: Transform.rotate(
+      //   angle: -math.pi / 2,
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab(),
+          ..._buildExpandingActionButtons(),
+          _buildTapToOpenFab(),
+        ],
+        // ),
+      ),
+    );
+  }
+
+  Widget _buildTapToCloseFab() {
+    return SizedBox(
+      width: 56.0,
+      height: 56.0,
+      child: Center(
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4.0,
+          child: InkWell(
+            onTap: _toggle,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.close,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildExpandingActionButtons() {
+    final children = <Widget>[];
+    final count = widget.children.length;
+    final step = 90.0 / (count - 1);
+    for (var i = 0, angleInDegrees = 0.0;
+        i < count;
+        i++, angleInDegrees += step) {
+      children.add(
+        _ExpandingActionButton(
+          directionInDegrees: angleInDegrees,
+          maxDistance: widget.distance,
+          progress: _expandAnimation,
+          child: widget.children[i],
+        ),
+      );
+    }
+    return children;
+  }
+
+  Widget _buildTapToOpenFab() {
+    return IgnorePointer(
+      ignoring: _open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        transform: Matrix4.diagonal3Values(
+          _open ? 0.7 : 1.0,
+          _open ? 0.7 : 1.0,
+          1.0,
+        ),
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: _open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: GestureDetector(
+            onTap: _toggle,
+            onLongPress: () {
+              print("long");
+            },
+            child: FloatingActionButton(
+              elevation: 4.0,
+              onPressed: _toggle,
+              child: const Icon(Icons.create),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ExpandingActionButton extends StatelessWidget {
+  const _ExpandingActionButton({
+    required this.directionInDegrees,
+    required this.maxDistance,
+    required this.progress,
+    required this.child,
+  });
+
+  final double directionInDegrees;
+  final double maxDistance;
+  final Animation<double> progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress,
+      builder: (context, child) {
+        final offset = Offset.fromDirection(
+          directionInDegrees * (math.pi / 180.0),
+          progress.value * maxDistance,
+        );
+        return Positioned(
+          right: 4.0 + offset.dx,
+          bottom: 4.0 + offset.dy,
+          child: Transform.rotate(
+            angle: (1.0 - progress.value) * math.pi / 2,
+            child: child!,
+          ),
+        );
+      },
+      child: FadeTransition(
+        opacity: progress,
+        child: child,
+      ),
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    this.onPressed,
+    required this.icon,
+  });
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Material(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: theme.colorScheme.secondary,
+      elevation: 4.0,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: theme.colorScheme.onSecondary,
+      ),
+    );
+  }
+}
+
+class FakeItem extends StatelessWidget {
+  const FakeItem({
+    required this.isBig,
+  });
+
+  final bool isBig;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+      height: isBig ? 128.0 : 36.0,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+        color: Colors.grey.shade300,
+      ),
+    );
+  }
+}
---
+++ example/lib/overlays/messanger_chathead.dart
@@ -0,0 +1,111 @@
+import 'package:fab_circular_menu/fab_circular_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+import 'package:flutter_overlay_window_example/floating_page.dart';
+
+import '../expandable_fab.dart';
+
+class MessangerChatHead extends StatefulWidget {
+  const MessangerChatHead({Key? key}) : super(key: key);
+
+  @override
+  State<MessangerChatHead> createState() => _MessangerChatHeadState();
+}
+
+class _MessangerChatHeadState extends State<MessangerChatHead> {
+  Color color = const Color(0xFFFFFFFF);
+  BoxShape shape = BoxShape.rectangle;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      // await FlutterOverlayWindow.updateFlag(OverlayFlag.clickThrough);
+    });
+    FlutterOverlayWindow.overlayListener.listen((event) {
+      if (event == 'HEY') {
+        // setState(() {
+        //  Update Timer
+        // });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ExpandableFab(
+        distance: 60.0,
+        children: [
+          ActionButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.format_size),
+          ),
+          ActionButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.insert_photo),
+          ),
+        ],
+      ),
+    );
+
+    return FabCircularMenu(ringDiameter: 160, ringWidth: 50, children: <Widget>[
+      IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {
+            print('Home');
+          }),
+      IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {
+            print('Favorite');
+          }),
+      IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {
+            print('Favorite');
+          })
+    ]);
+    return FloatingPage();
+    // return FloatingActionButton(
+    //   onPressed: () {},
+    //   child: Text("hi"),
+    // );
+    return Material(
+      color: Colors.transparent,
+      elevation: 0.0,
+      child: GestureDetector(
+        onTap: () async {
+          if (shape == BoxShape.circle) {
+            // await FlutterOverlayWindow.resizeOverlay(matchParent, matchParent);
+            // setState(() {
+            //   shape = BoxShape.rectangle;
+            // });
+            await FlutterOverlayWindow.resizeOverlay(250, 250);
+            setState(() {
+              shape = BoxShape.rectangle;
+            });
+          } else {
+            await FlutterOverlayWindow.resizeOverlay(350, 350);
+            setState(() {
+              shape = BoxShape.circle;
+            });
+          }
+        },
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Hi'),
+        ),
+
+        // child: Container(
+        //   height: MediaQuery.of(context).size.height,
+        //   decoration: BoxDecoration(color: color, shape: shape),
+        //   child: const Center(
+        //     child: FlutterLogo(),
+        //   ),
+        // ),
+      ),
+    );
+  }
+}
