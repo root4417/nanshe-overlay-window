f6621a404d9a6a5a8af78754c212bddfa5cd8310
Amapps58 <104589233+Amapps58@users.noreply.github.com>
2023-03-21 21:54:51 +0330

    updated package name

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
@@ -6,7 +6,7 @@ import android.view.View;
 import io.flutter.plugin.common.MethodChannel;
 
 final public class OverlayConstants {
-    static final String APP_PACKAGE = "io.hexasoft.nanshe";
+    static final String APP_PACKAGE = "io.hexasoft.themisse";
 //    static final String APP_PACKAGE = "com.example.flutter_overlay_window_example";
     static final String CACHED_TAG = "myCachedEngine";
     static final String CHANNEL_TAG = "x-slayer/overlay_channel";
